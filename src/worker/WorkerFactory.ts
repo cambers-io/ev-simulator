667c4cbe2673093443e63641636e9273cddb4e66
Samuel Chen <samuel@cambers.io>
2025-07-17 17:57:09 -0700

    Delete Dynamic pool files

--- src/worker/WorkerFactory.ts
+++ src/worker/WorkerFactory.ts
@@ -4,8 +4,6 @@ import { WorkerOptions, WorkerProcessType } from '../types/Worker';
 import Constants from '../utils/Constants';
 import { PoolOptions } from 'poolifier';
 import WorkerAbstract from './WorkerAbstract';
-import WorkerDynamicPool from './WorkerDynamicPool';
-import WorkerSet from './WorkerSet';
 import WorkerStaticPool from './WorkerStaticPool';
 
 export default class WorkerFactory {
@@ -23,18 +21,9 @@ export default class WorkerFactory {
     options?.messageHandler && (options.poolOptions.messageHandler = options.messageHandler);
     let workerImplementation: WorkerAbstract = null;
     switch (workerProcessType) {
-      case WorkerProcessType.WORKER_SET:
-        options.elementsPerWorker = options.elementsPerWorker ?? Constants.DEFAULT_CHARGING_STATIONS_PER_WORKER;
-        workerImplementation = new WorkerSet<T>(workerScript, options.elementsPerWorker, options.startDelay, options);
-        break;
       case WorkerProcessType.STATIC_POOL:
         options.poolMaxSize = options.poolMaxSize ?? Constants.DEFAULT_WORKER_POOL_MAX_SIZE;
         workerImplementation = new WorkerStaticPool<T>(workerScript, options.poolMaxSize, options.startDelay, options.poolOptions);
-        break;
-      case WorkerProcessType.DYNAMIC_POOL:
-        options.poolMinSize = options.poolMinSize ?? Constants.DEFAULT_WORKER_POOL_MIN_SIZE;
-        options.poolMaxSize = options.poolMaxSize ?? Constants.DEFAULT_WORKER_POOL_MAX_SIZE;
-        workerImplementation = new WorkerDynamicPool<T>(workerScript, options.poolMinSize, options.poolMaxSize, options.startDelay, options.poolOptions);
         break;
       default:
         throw new Error(`Worker implementation type '${workerProcessType}' not found`);
